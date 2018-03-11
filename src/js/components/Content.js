c02248eeda2bf805ff9e79e5a537fcb318cae139
Alex <alexofrontend@gmail.com>
2018-03-10 19:34:07 -0500

    content to landing name change

--- src/js/components/Content.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Content = () => {
-  return <div className="center" id="landing">
-SIMPLE REACT BOILERPLATE
-</div>
-
-};
-export default Content;
---
+++ src/js/components/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Landing = () => {
+  return <div id="home">
+    <div className="center" id="landing">
+      <div className="center-item">
+        <a className="ldn">A Simple React Boilerplate</a>
+        <div className="gitmenu">
+          <a className="github-button" href="https://github.com/aleodev/Simple-React-Boilerplate" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star aleodev/Simple-React-Boilerplate on GitHub">Star</a>
+          <br />
+          <a className="github-button" href="https://github.com/aleodev/Simple-React-Boilerplate" data-size="large" data-show-count="true" aria-label="Follow @aleodev on GitHub">Follow @aleodev</a>
+        </div>
+      </div>
+    </div>
+  </div>
+};
+export default Landing;
