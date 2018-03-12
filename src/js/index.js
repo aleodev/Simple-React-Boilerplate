cbf68b1957b2128d1e9b581ea9cf107fdee182d4
Alex <alexofrontend@gmail.com>
2018-03-12 03:21:38 -0400

    removed jquery unless used and added env

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,21 @@
+/* global document */
+/* eslint-env browser */
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import TweenMax from 'gsap/src/minified/TweenMax.min'
-import $ from 'jquery/dist/jquery.min'
+// import $ from 'jquery/dist/jquery.min' [NOT BEING USED]
 import Landing from './components/Landing'
+// import '../sass/main.scss' [ONLY IN PRODUCTION]
 class Layout extends Component {
   constructor() {
     super()
     this.state = {}
   }
-  componentDidMount(){
-    TweenMax.from("#landing", 3, {y:100, autoAlpha:0})
+  componentDidMount() {
+    TweenMax.from("#landing", 3, {
+      y: 100,
+      autoAlpha: 0
+    })
   }
   render() {
     return (<div>
