06c9e6db212863e322b98d3219cd9ae0c8a7f7f2
Alex <alexofrontend@gmail.com>
2018-03-12 03:20:54 -0400

    fixed git icons

--- src/js/components/Landing.js
+++ src/js/components/Landing.js
@@ -6,9 +6,11 @@ const Landing = () => {
       <div className="center-item">
         <a className="ldn">A Simple React Boilerplate</a>
         <div className="gitmenu">
-          <a className="github-button" href="https://github.com/aleodev/Simple-React-Boilerplate" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star aleodev/Simple-React-Boilerplate on GitHub">Star</a>
-          <br />
-          <a className="github-button" href="https://github.com/aleodev/Simple-React-Boilerplate" data-size="large" data-show-count="true" aria-label="Follow @aleodev on GitHub">Follow @aleodev</a>
+          <a className="github-button" href="https://github.com/aleodev/Simple-React-Boilerplate"
+           data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star aleodev/Simple-React-Boilerplate on GitHub">Star</a>
+          <br/>
+          <a className="github-button" href="https://github.com/aleodev/Simple-React-Boilerplate"
+          data-size="large" data-show-count="true" aria-label="Follow @aleodev on GitHub">Follow @aleodev</a>
         </div>
       </div>
     </div>
